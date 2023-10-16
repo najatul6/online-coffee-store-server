35699092bf0514b793ed7992a1b3a050340a13dd
najatul6 <mdnajatulislam.develop@gmail.com>
2023-10-17 05:01:40 +0600

    Add User and remove users

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const userCollection = client.db('coffeeDB').collection('user')
 
     app.get('/coffee', async(req, res)=>{
         const cursor = coffeeCollection.find();
@@ -76,6 +77,26 @@ async function run() {
         const query = {_id: new ObjectId(id)};
         const result = await coffeeCollection.deleteOne(query);
         res.send(result)
+    })
+
+    // User related API 
+    app.get('/user', async(req, res)=>{
+      const cursor = userCollection.find();
+      const user = await cursor.toArray();
+      res.send(user)
+    })
+
+    app.post('/user', async(req, res)=>{
+      const user = req.body
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+    
+    app.delete('/user/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
     })
 
 
